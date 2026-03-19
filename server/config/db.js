11e0cfbd75f0ed3dc78f7926a3e7dee10f6e5f31
John Dave Villasan <dave.uc.main.it@gmail.com>
2026-03-19 22:53:05 +0800

    added getconnection checking

--- server/config/db.js
+++ server/config/db.js
@@ -10,13 +10,14 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-pool.getConnection()
-  .then(conn => {
-    console.log("Database connected!");
+pool
+  .getConnection()
+  .then((conn) => {
+    console.log('Database connected!');
     conn.release();
   })
-  .catch(err => {
-    console.error("Database connection failed:", err);
+  .catch((err) => {
+    console.error('Database connection failed:', err);
   });
 
 module.exports = pool;
